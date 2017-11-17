a3cedfcefa93b389c12cb6401e82fba9152b6ca8
Abhijit <abhijitkghosh29@gmail.com>
2017-11-16 21:20:30 -0500

    post method added

--- express.js
+++ express.js
@@ -1,58 +1,65 @@
 var express = require("express");
+var path = require('path');
+var bodyParser = require ("body-parser");
 
 var app = express();
-var PORT = 3000;
-var path = require('path');
+var PORT = process.env.PORT || 3000;
+
+
+
 
 app.listen(PORT,function(){
 	console.log("App listening on port "+ PORT);
 })
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var input = [{
-	routename:"guest1",
+	
 	Name:"Guest1",
 	Number : "123456788",
 	email_ID : "abc@abc.com",
 	unique_ID : "Unique1"
 },
 {
-routename:"guest2",
+
 	Name:"Guest2",
 	Number : "123456788",
 	email_ID : "abc2@abc2.com",
 	unique_ID : "Unique2"
 },
 {
-routename:"guest3",
+
 	Name:"Guest3",
 	Number : "123456788",
 	email_ID : "abc3@abc3.com",
 	unique_ID : "Unique3"
 },
 {
-routename:"guest4",
+
 	Name:"Guest4",
 	Number : "123456788",
 	email_ID : "abc4@abc4.com",
 	unique_ID : "Unique4"
 },
 {
-routename:"guest5",
+
 	Name:"Guest5",
 	Number : "123456788",
 	email_ID : "abc5@abc5.com",
 	unique_ID : "Unique5"
 },
 {
-routename:"guest6",
+
 	Name:"Guest6",
 	Number : "123456788",
 	email_ID : "abc6@abc6.com",
 	unique_ID : "Unique6"
 },
 {
-	routename:"guest7",
+
 	Name:"Guest7",
 	Number : "123456788",
 	email_ID : "abc7@abc7.com",
@@ -121,3 +128,17 @@ app.get("/api/waitList",function(req,res){
          return res.json(waitList);
       //}
  })
+
+
+app.post("/api/new", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newcharacter = req.body;
+  //newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  input.push(newcharacter);
+
+  res.json(newcharacter);
+});
